6a5b7d4945942c3d25716713142df1a7d06678d2
cassianobraz <cassanopb@gmail.com>
2026-05-08 23:48:53 -0400

    feat: new structure and redis register

---
+++ src/HybridCache.Api/HybridCache.Api/Controllers/HybridController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Hybrid;
+
+namespace Hybrid.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HybridController : ControllerBase
+{
+    private readonly HybridCache _hybridCache;
+
+    public HybridController(HybridCache hybridCache)
+    {
+        _hybridCache = hybridCache;
+    }
+
+    [HttpGet("get-using-cache/{name}/{age}")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetUsingCache(
+        [FromRoute] string name,
+        [FromRoute] long age,
+        CancellationToken token)
+    {
+        var result = await _hybridCache.GetOrCreateAsync(
+            key: $"{name}-{age}",
+            async cancel => await GetDataFromTheSourceAsync(name, age, cancel),
+            cancellationToken: token
+        );
+
+        return Ok(result);
+    }
+
+    [HttpGet("get-using-cache/v2/{name}/{age}")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetUsingCacheV2(
+        [FromRoute] string name,
+        [FromRoute] long age,
+        CancellationToken token)
+    {
+        var tags = new List<string> { "tag1", "tag2", "tag3" };
+
+        var entryOptions = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromMinutes(1),
+            LocalCacheExpiration = TimeSpan.FromMinutes(1)
+        };
+
+        var result = await _hybridCache.GetOrCreateAsync(
+            key: $"{name}-{age}",
+            factory: async cancel => await GetDataFromTheSourceAsync(name, age, cancel),
+            options: entryOptions,
+            tags: tags,
+            cancellationToken: token
+        );
+
+        return Ok(result);
+    }
+
+    private static async Task<string> GetDataFromTheSourceAsync(
+        string name,
+        long age,
+        CancellationToken token)
+    {
+        await Task.Delay(5000, token);
+
+        return $"canalDEPLOY-{name}-{age}";
+    }
+}
--- src/HybridCache.Api/HybridCache.Api/Program.cs
+++ src/HybridCache.Api/HybridCache.Api/Program.cs
@@ -1,9 +1,29 @@
+using Microsoft.Extensions.Caching.Hybrid;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddHybridCache(options =>
+{
+    options.ReportTagMetrics = true;
+    options.MaximumPayloadBytes = 1024 * 1024;
+    options.MaximumKeyLength = 1024;
+    options.DefaultEntryOptions = new HybridCacheEntryOptions
+    {
+        Expiration = TimeSpan.FromSeconds(5),
+        LocalCacheExpiration = TimeSpan.FromSeconds(5)
+    };
+});
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration =
+        builder.Configuration.GetConnectionString("RedisConnectionString");
+});
 
 var app = builder.Build();
 
